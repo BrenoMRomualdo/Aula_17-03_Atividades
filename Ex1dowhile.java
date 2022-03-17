bb1c4880e7a42bf4bb032943f3b65668d713dfed
BrenoMR <aluno@IPIRANGA.FATEC>
2022-03-17 18:05:05 -0300

    Terceiro commit, ex1 parte 3 feita

--- Ex1dowhile.java
+++ Ex1dowhile.java
@@ -1,3 +1,11 @@
 public class Ex1dowhile {
-    
+    public static void main(String[] args) {
+        int numero_natural = 0;
+        int contador = 1;
+        do{numero_natural++;
+            System.out.printf("%d  ", numero_natural);
+            contador++;
+        }
+        while(contador <= 100);
+    }
 }
--- Ex1while.java
+++ Ex1while.java
@@ -2,7 +2,7 @@ public class Ex1while{
     public static void main(String[] args) {
         int numero_natural = 0;
         int contador = 1;
-        while(contador <= 101){
+        while(contador <= 100){
             numero_natural++;
             System.out.printf("%d  ", numero_natural);
             contador++;
