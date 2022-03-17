610c35a6d4da223093cd36e3209bf712933f09de
Aluno <aluno@IPIRANGA.FATEC>
2022-03-17 17:58:19 -0300

    primeiro commit, ex1 parte 1 finalizada

---
+++ Ex1for.java
@@ -0,0 +1,10 @@
+public class Ex1for{
+    public static void main(String[] args) {
+        int numero_natural = 0;
+        for (int contador = 1; contador <= 200; contador++){
+            numero_natural++;
+            System.out.printf("%d  ", numero_natural);
+            contador++;
+        }
+    }
+}
